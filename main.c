de547682e411dcd2cf0b388348830a9b0312396b
MohAlshahrani <90883554+MohAlshahrani@users.noreply.github.com>
2022-11-10 19:26:55 -0500

    pointers corrected

--- main.c
+++ main.c
@@ -2,13 +2,13 @@
 #include <stdlib.h>
 #include <omp.h>
 
-int * PrefixSum(int In[], int size);
+int * PrefixSum(int *In, int size);
 
 int main() {
 
     double elt = omp_get_wtime();
-    int arr[1000];
-    int size = sizeof(arr)/sizeof(arr[0]);
+    int *arr;
+    int size = 1000;
     for( int i=0 ; i<size; i++){
         arr[i] = rand()%100;
     }
@@ -26,7 +26,7 @@ int main() {
     return 0;
 
 }
-int * PrefixSum(int In[], int size) {
+int * PrefixSum(int *In, int size) {
 #pragma omp parallel while private(i) reduction(+:In,sum,out)
     int i = 0;
     int sum ;
@@ -35,7 +35,7 @@ int * PrefixSum(int In[], int size) {
     Out[0] = sum;
     while (i<size){
         i++;
-        sum = *(In+i)+sum;
+        sum = *In[i]+sum;
         Out[i] = sum;
     }
     int *OutPtr;
