8da1f4400c630a4d994991e2ca60729453ca549a
MohAlshahrani <90883554+MohAlshahrani@users.noreply.github.com>
2022-11-10 21:17:56 -0500

    pointers corrected

--- main.c
+++ main.c
@@ -6,7 +6,7 @@ int * PrefixSum(int In[], int size);
 
 int main() {
 
-    double elt = omp_get_wtime();
+    float elt = omp_get_wtime();
     int arr[1000];
     int size = sizeof(arr)/sizeof(arr[0]);
     for( int i=0 ; i<size; i++){
@@ -17,11 +17,14 @@ int main() {
     int *Result;
 
     Result = PrefixSum(arr, size);
-
+/*
     for (int i = 0; i < size; i++) {
         printf("result[%d] : %d\n", i, Result[i]);
     }
+    */
+
     elt = omp_get_wtime() - elt;
+    printf("elt = %f",elt)
     return 0;
 
 }
